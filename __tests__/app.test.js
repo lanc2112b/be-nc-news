f808f224cf8c0810e32bc8f48939660ee963193f
Tony Lancaster <lanc1968@gmail.com>
2023-02-23 16:40:29 +0000

    Change separate tests for t11 & 5.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -55,8 +55,31 @@ describe("GET Endpoints", () => {
     });
   });
 
-  describe("GET /api/articles/ (05)(11)", () => {
+  describe("GET /api/articles/ (05)", () => {
     it("200: Returns object with a single article, selected by id", () => {
+      return request(app)
+        .get("/api/articles/3")
+        .expect(200)
+        .then((response) => {
+          const { article } = response.body;
+          expect(article).toBeInstanceOf(Object);
+          expect(article.article_id).toBe(3);
+            expect(article).toMatchObject({
+              article_id: expect.any(Number),
+              created_at: expect.any(String),
+              title: expect.any(String),
+              author: expect.any(String),
+              votes: expect.any(Number),
+              topic: expect.any(String),
+              article_img_url: expect.any(String),
+              comment_count: expect.any(String),
+            });
+        });
+    });
+  });
+
+  describe("GET /api/articles/ (11)", () => {
+    it("200: Returns object with a single article, selected by id to include comment_count", () => {
       return request(app)
         .get("/api/articles/1")
         .expect(200)
@@ -64,16 +87,16 @@ describe("GET Endpoints", () => {
           const { article } = response.body;
           expect(article).toBeInstanceOf(Object);
           expect(article.article_id).toBe(1);
-          expect(article.comment_count).toBe('11');
-          expect(article.author).toBe('butter_bridge');
+          expect(article.comment_count).toBe("11");
+          expect(article.author).toBe("butter_bridge");
           expect(article.title).toBe("Living in the shadow of a great man");
-          expect(article.topic).toBe('mitch');
-            expect(article).toMatchObject({
-              article_id: expect.any(Number),
-              created_at: expect.any(String),
-              votes: expect.any(Number),
-              article_img_url: expect.any(String),
-            });
+          expect(article.topic).toBe("mitch");
+          expect(article).toMatchObject({
+            article_id: expect.any(Number),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+          });
         });
     });
   });
