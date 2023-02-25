1c401ba54cfceef840c9466f28a5be93081134cf
Tony Lancaster <lanc1968@gmail.com>
2023-02-25 13:26:18 +0000

    Remove additional empty lines

--- models/commentModel.js
+++ models/commentModel.js
@@ -96,11 +96,6 @@ exports.updateCommentById = (id, update) => {
     });
 };
 
-
-
-
-
-
 exports.deleteCommentById = (id) => {
   return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [id])
     .then((result) => {
