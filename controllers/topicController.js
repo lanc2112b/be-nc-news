d478ff9f2656933c43ad62b1c0ccc5f0049ea120
Tony Lancaster <lanc1968@gmail.com>
2023-02-25 12:17:32 +0000

    T22 Add POST /api/topics endpoint. Add insertNewTopic to topicModel. Add postNewTopic to topicController. Add route POST to topics-router. Add tests for adding new topic. Add errorHandler for duplicate inputs. Add tests for missing slug, description. Add tests for duplicate input on primary key. All tests pass.

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,4 +1,4 @@
-const { selectAllTopics } = require('../models/topicModel');
+const { selectAllTopics, insertNewTopic } = require("../models/topicModel");
 
 /** Imports & BP above here */
 
@@ -11,4 +11,18 @@ exports.getTopics = (request, response, next) => {
     .catch((error) => {
       next(error);
   });
+}
+
+exports.postNewTopic = (request, response, next) => {
+
+  const { slug, description } = request.body;
+  //console.log(slug, description);
+  insertNewTopic(slug, description)
+    .then((result) => {
+      response.status(201).send({ topic: result });
+    })
+    .catch((error) => {
+      next(error);
+    });
+
 }
